beb6ccd64d7f52e55a99effcb07719f46709e683
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-22 23:20:54 +0100

    build(KAN-36): initialize Spring Boot application

---
+++ src/main/java/fr/kolos/Kolos/KolosApplication.java
@@ -0,0 +1,13 @@
+package fr.kolos.Kolos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KolosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/kolos/Kolos/KolosApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.kolos.Kolos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
