0743021e2d74db552dcc9c0ea16deb1f43eafcee
jihane <jihanfaid25@gmail.com>
2024-04-04 15:19:18 +0200

    feat(KAN-38): add PutMuscleController

---
+++ src/main/java/fr/kolos/presentation/controller/muscle/PutMuscleController.java
@@ -0,0 +1,29 @@
+package fr.kolos.presentation.controller.muscle;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import fr.kolos.business.dto.MuscleDto;
+import fr.kolos.persistence.repository.IMuscleRepository;
+
+public class PutMuscleController {
+
+    private IMuscleRepository service;
+
+    public IMuscleRepository getService() {
+        return service;
+    }
+
+    public void setService(IMuscleRepository service) {
+        this.service = service;
+    }
+
+
+
+    @PutMapping("/muscles")
+    public MuscleDto PutMuscle(@RequestBody final MuscleDto muscleDto){
+        return service.PutMuscle();
+
+    }
+
+}
