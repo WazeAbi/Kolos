6334d592c837e45eb9bf6ffe66f1283c6390c0de
jihane <jihanfaid25@gmail.com>
2024-04-04 14:32:52 +0200

    feat(KAN-38): add MuscleDto

---
+++ src/main/java/fr/kolos/business/dto/MuscleDto.java
@@ -0,0 +1,25 @@
+package fr.kolos.business.dto;
+
+public class MuscleDto {
+
+    private int idMuscle;
+
+    private String nameMuscle;
+
+    public int getIdMuscle() {
+        return idMuscle;
+    }
+
+    public void setIdMuscle(int idMuscle) {
+        this.idMuscle = idMuscle;
+    }
+
+    public String getNameMuscle() {
+        return nameMuscle;
+    }
+
+    public void setNameMuscle(String nameMuscle) {
+        this.nameMuscle = nameMuscle;
+    } 
+    
+}
