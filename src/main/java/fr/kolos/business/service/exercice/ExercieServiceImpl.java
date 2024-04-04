b8089c8842e075165f6842ef0a44c12e7c9f894f
WazeAbi <alexdalle12@gmail.com>
2024-04-04 14:11:13 +0200

    Feat(KAN-39):Add 2 methods

--- src/main/java/fr/kolos/business/service/exercice/ExercieServiceImpl.java
+++ src/main/java/fr/kolos/business/service/exercice/ExercieServiceImpl.java
@@ -7,10 +7,24 @@ import fr.kolos.persistence.repository.IExerciceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ExercieServiceImpl implements IExerciceService {
     private IExerciceRepository repoEx;
     private ExerciceConvert convertEx;
+
+    @Override
+    public List<ExerciceDto> ListExercises() {
+        final List<Exercice> result = repoEx.findAll();
+        return ExerciceConvert.getInstance().convertListEntityToListDto(result);
+    }
+
+
+    @Override
+    public ExerciceDto getExerciseById(int id) {
+        return convertEx.convertEntityToDto(repoEx.getReferenceById(id));
+    }
 
     @Override
     public void modifyExercice(ExerciceDto exercice, int id) {
