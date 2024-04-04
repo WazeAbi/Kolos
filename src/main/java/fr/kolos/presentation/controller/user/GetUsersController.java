e8c39e17562a1da9dde910c784c7e872f570f192
NicolasDrp <drapier.nicolas07@gmail.com>
2024-04-04 11:08:53 +0200

    feat(KAN-37): add GetUsersController

--- src/main/java/fr/kolos/presentation/controller/user/GetUsersController.java
+++ src/main/java/fr/kolos/presentation/controller/user/GetUsersController.java
@@ -1,5 +1,43 @@
 package fr.kolos.presentation.controller.user;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.kolos.business.dto.UserDto;
+import fr.kolos.business.service.user.IUserService;
+
+
+/**
+ * Controller class for handling requests to retrieve all users.
+ */
+@RestController
+@CrossOrigin
 public class GetUsersController {
 
+	private IUserService service;
+
+	/**
+	 * Retrieves all users.
+	 *
+	 * @return A list of DTOs representing all users.
+	 */
+	@GetMapping("/users")
+	public List<UserDto> getUsers() {
+		return service.getUsers();
+	}
+
+	/**
+	 * Sets the user service for this controller.
+	 *
+	 * @param service The user service to be set.
+	 */
+	@Autowired
+	public void setService(IUserService service) {
+		this.service = service;
+	}
+	
 }
