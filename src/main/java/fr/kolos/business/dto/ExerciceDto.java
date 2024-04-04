1000e73d05151b48fb6d6f49eee1e77ab3dfbc53
WazeAbi <alexdalle12@gmail.com>
2024-04-04 11:33:55 +0200

    Feat(KAN-39):Creation ExerciceDto

--- src/main/java/fr/kolos/business/dto/ExerciceDto.java
+++ src/main/java/fr/kolos/business/dto/ExerciceDto.java
@@ -1,2 +1,40 @@
-package fr.kolos.business.dto;public class ExerciceDto {
+package fr.kolos.business.dto;
+
+public class ExerciceDto {
+    private int idEx;
+    private String nameExercice;
+    private String description;
+    private String urlImage;
+
+    public int getIdEx() {
+        return idEx;
+    }
+
+    public void setIdEx(int idEx) {
+        this.idEx = idEx;
+    }
+
+    public String getNameExercice() {
+        return nameExercice;
+    }
+
+    public void setNameExercice(String nameExercice) {
+        this.nameExercice = nameExercice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
 }
