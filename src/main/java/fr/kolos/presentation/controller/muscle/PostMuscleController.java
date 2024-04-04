8aae391f06c9ea7fcea25c0756fbbb4479cf0b26
jihane <jihanfaid25@gmail.com>
2024-04-04 15:18:12 +0200

    feat(KAN-38): add PostMuscleController

---
+++ src/main/java/fr/kolos/presentation/controller/muscle/PostMuscleController.java
@@ -0,0 +1,30 @@
+package fr.kolos.presentation.controller.muscle;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.kolos.business.dto.MuscleDto;
+import fr.kolos.business.service.muscle.IMuscleService;
+
+@RestController
+@CrossOrigin
+public class PostMuscleController {
+
+    private IMuscleService service;
+
+    @PostMapping("/muscles")
+    public MuscleDto PostMuscle(@RequestBody final MuscleDto muscleDto) {
+        return service.PostMuscle(muscleDto);
+    }
+
+    public IMuscleService getService() {
+        return service;
+    }
+
+    public void setService(IMuscleService service) {
+        this.service = service;
+    }
+
+}
