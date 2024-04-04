68a9f881aa62ef9e45e2698c9114af256cd53826
WazeAbi <alexdalle12@gmail.com>
2024-04-04 13:59:27 +0200

    Feat(KAN-39):Add 2 new methods on IExerciceService

--- src/main/java/fr/kolos/business/service/exercice/IExerciceService.java
+++ src/main/java/fr/kolos/business/service/exercice/IExerciceService.java
@@ -4,8 +4,12 @@ import fr.kolos.business.dto.ExerciceDto;
 import fr.kolos.persistence.entity.Exercice;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface IExerciceService {
+    List<ExerciceDto> ListExercises();
+    ExerciceDto getExerciseById(final int id);
     void modifyExercice(ExerciceDto exercice, int id);
     void deleteExercice(int id);
     void createExercice(ExerciceDto exercice);
