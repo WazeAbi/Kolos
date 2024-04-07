f3ba2945ab7f26bbdd9877860ceb179ceb550d30
NicolasDrp <drapier.nicolas07@gmail.com>
2024-04-07 19:21:08 +0200

    feat(KAN-40): add LoadUserByUsername from UserDetailsService

--- src/main/java/fr/kolos/business/service/user/UserServiceImpl.java
+++ src/main/java/fr/kolos/business/service/user/UserServiceImpl.java
@@ -1,17 +1,29 @@
 package fr.kolos.business.service.user;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import fr.kolos.business.convert.UserConvert;
 import fr.kolos.business.dto.UserDto;
+import fr.kolos.persistence.entity.User;
 import fr.kolos.persistence.repository.IUserRepository;
 
 @Service
-public class UserServiceImpl implements IUserService {
+public class UserServiceImpl implements IUserService, UserDetailsService {
 
 	private IUserRepository repo;
+	
+	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
 
 	@Override
 	public List<UserDto> getUsers() {
@@ -23,6 +35,7 @@ public class UserServiceImpl implements IUserService {
 		return UserConvert.getInstance().convertEntityToDto(repo.getReferenceById(id));
 	}
 
+	//TODO encode password
 	@Override
 	public UserDto postUser(UserDto userDto) {
 		return UserConvert.getInstance()
@@ -32,6 +45,22 @@ public class UserServiceImpl implements IUserService {
 	@Override
 	public void deleteUser(UserDto userDto) {
 		repo.delete(UserConvert.getInstance().convertDtoToEntity(userDto));
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) {
+		User user = repo.loadUserByEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found with email: " + email);
+		}
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+				getAuthorities(user));
+	}
+
+	private Set<GrantedAuthority> getAuthorities(User user) {
+		Set<GrantedAuthority> authorities = new HashSet<>();
+		authorities.add(new SimpleGrantedAuthority(user.getRoleUser()));
+		return authorities;
 	}
 
 	@Autowired
