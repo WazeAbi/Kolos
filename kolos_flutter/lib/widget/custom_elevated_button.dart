7d58731a43d0bf6f62f87ae6098dfe9002d4a3f3
NicolasDrp <drapier.nicolas07@gmail.com>
2024-06-29 20:28:23 +0200

    feat(KAN-54) : create un evlevated button as a component

---
+++ kolos_flutter/lib/widget/custom_elevated_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  const CustomElevatedButton(
+      {super.key,
+      required this.text,
+      required this.callback,
+      required this.isDark});
+
+  final bool isDark;
+  final String text;
+  final Function callback;
+
+  @override
+  Widget build(BuildContext context) {
+    ElevatedButton button;
+    button = ElevatedButton(
+      onPressed: () {
+        callback();
+      },
+      style: ButtonStyle(
+          elevation: WidgetStateProperty.all<double?>(5),
+          backgroundColor: WidgetStatePropertyAll<Color>(isDark
+              ? Theme.of(context).colorScheme.onSecondary
+              : Theme.of(context).colorScheme.primary),
+          // padding:
+          //     MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.all(0)),
+          fixedSize: WidgetStatePropertyAll<Size>(Size(
+              MediaQuery.of(context).size.width * 0.6,
+              MediaQuery.of(context).size.width * 0.1))),
+      child: Text(
+        text,
+        style: isDark
+            ? TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.w500)
+            : TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.secondary,
+                fontWeight: FontWeight.w500),
+      ),
+    );
+    return button;
+  }
+}
