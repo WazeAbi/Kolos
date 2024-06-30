3d206747ae64b4a4b14e8ada4c77f9ec65f10a18
NicolasDrp <drapier.nicolas07@gmail.com>
2024-06-30 21:50:41 +0200

    fix(KAN-50) : add the possibility for the user to register

--- kolos_flutter/lib/screen/register.dart
+++ kolos_flutter/lib/screen/register.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:http/http.dart' as http;
 import 'package:kolos_flutter/widget/custom_back_button.dart';
 import 'package:kolos_flutter/widget/custom_elevated_button.dart';
 import 'package:kolos_flutter/widget/custom_input.dart';
@@ -20,6 +22,11 @@ class _RegisterState extends State<Register> {
   final TextEditingController _passwordConfirmController =
       TextEditingController();
   bool cguChecked = false;
+  bool isLoading = false;
+
+  final _emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+$');
+  final _passwordRegex =
+      RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$');
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +44,7 @@ class _RegisterState extends State<Register> {
               height: 15,
             ),
             CustomInput(
-              label: "Nom",
+              label: "Nom d'utilisateur",
               controller: _usernameController,
               isPassword: false,
             ),
@@ -98,11 +105,15 @@ class _RegisterState extends State<Register> {
               ],
             ),
             const SizedBox(height: 20),
-            CustomElevatedButton(
-              text: "Inscription",
-              callback: validateForm,
-              isDark: true,
-            ),
+            isLoading
+                ? CircularProgressIndicator(
+                    color: Theme.of(context).colorScheme.onSecondary,
+                  )
+                : CustomElevatedButton(
+                    text: "Inscription",
+                    callback: validateForm,
+                    isDark: true,
+                  ),
             const SizedBox(height: 60),
             RichText(
               text: TextSpan(
@@ -138,5 +149,93 @@ class _RegisterState extends State<Register> {
     );
   }
 
-  void validateForm() {}
+  void validateForm() async {
+    final String username = _usernameController.text;
+    final String email = _emailController.text;
+    final String emailConfirm = _emailConfirmController.text;
+    final String password = _passwordController.text;
+    final String passwordConfirm = _passwordConfirmController.text;
+
+    if (username.isEmpty) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Le nom est requis.')));
+      return;
+    }
+
+    if (!cguChecked) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Vous devez accepter les CGU pour continuer.')));
+      return;
+    }
+
+    if (email != emailConfirm) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Les emails ne correspondent pas.')));
+      return;
+    }
+
+    if (!_emailRegex.hasMatch(email)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Adresse email invalide.')));
+      return;
+    }
+
+    if (password != passwordConfirm) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Les mots de passe ne correspondent pas.')));
+      return;
+    }
+
+    if (!_passwordRegex.hasMatch(password)) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Le mot de passe doit contenir au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un caractère spécial.')));
+      return;
+    }
+
+    setState(() {
+      isLoading = true;
+    });
+
+    final Map<String, dynamic> body = {
+      "username": username,
+      "email": email,
+      "password": password,
+      "roleUser": "USER"
+    };
+
+    try {
+      final response = await http.post(
+        Uri.parse('http://localhost:8080/register'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(body),
+      );
+
+      if (!mounted) return;
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Inscription réussie !')));
+        context.go('/login'); // Rediriger vers la page de connexion
+      } else if (response.statusCode == 409 &&
+          response.body.contains('Email already exists')) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Cet email est déjà utilisé.')));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Erreur lors de l\'inscription.')));
+      }
+    } catch (e) {
+      if (!mounted) return;
+
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Erreur : $e')));
+    } finally {
+      if (mounted) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    }
+  }
 }
