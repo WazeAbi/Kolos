57b175e056988cdf92ebfe47d3bd1c07b3d55e9f
NicolasDrp <drapier.nicolas07@gmail.com>
2024-06-30 21:28:22 +0200

    fix(KAN-50) : return a custom response if the user email is already used

--- src/main/java/fr/kolos/presentation/controller/user/RegisterController.java
+++ src/main/java/fr/kolos/presentation/controller/user/RegisterController.java
@@ -1,6 +1,8 @@
 package fr.kolos.presentation.controller.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,12 +24,19 @@ public class RegisterController {
 	 * Creates a new user.
 	 *
 	 * @param userDto The DTO representing the user to be created.
+	 * @return 
 	 * @return The DTO representing the newly created user.
 	 * @throws IllegalAccessException
 	 */
 	@PostMapping("/register")
-	public UserDto postUser(@RequestBody final UserDto userDto) {
-		return service.postUser(userDto);
+	public  ResponseEntity<String> postUser(@RequestBody final UserDto userDto) {
+//		return service.postUser(userDto);
+		try {
+			service.postUser(userDto);
+            return ResponseEntity.ok("User registered successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already exists");
+        }
 	}
 
 	/**
